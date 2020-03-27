3042fea2ddd339f9cf78aa3eccb3c2eb9cb8f78d
Sam Wyant <samuule2@gmail.com>
2020-03-26 19:00:01 -0500

    Added code for equal numbers

--- program.py
+++ program.py
@@ -4,4 +4,6 @@ num2 = int(input("Number 2: "))
 if (num1 > num2):
     print("Num1 is greater than Num2")
 elif (num1 < num2):
-    print("Num2 is greater than Num1")
+    print("Number 2 is greater than Number 1")
+else
+    print("Number 1 and Number 2 are equal"
